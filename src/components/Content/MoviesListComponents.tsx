6a6c5942f7fc979f4c879fc95d28212899a7a390
Nikita Zhulev <Nikita_Zhulev@epam.com>
2022-03-10 11:10:39 +0300

    add more movies to mock data

--- src/components/Content/MoviesListComponents.tsx
+++ src/components/Content/MoviesListComponents.tsx
@@ -13,7 +13,70 @@ export default function MovieListComponents(props: ContentProps) {
                 releaseDate={props.moviesData[1].release_date}
                 genre={props.moviesData[1].genres[0]}
             />
-            <MovieCard screen={props.screen} setScreen={props.setScreen} />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[2].title}
+                posterPath={props.moviesData[2].poster_path}
+                releaseDate={props.moviesData[2].release_date}
+                genre={props.moviesData[2].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[3].title}
+                posterPath={props.moviesData[3].poster_path}
+                releaseDate={props.moviesData[3].release_date}
+                genre={props.moviesData[3].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[4].title}
+                posterPath={props.moviesData[4].poster_path}
+                releaseDate={props.moviesData[4].release_date}
+                genre={props.moviesData[4].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[5].title}
+                posterPath={props.moviesData[5].poster_path}
+                releaseDate={props.moviesData[5].release_date}
+                genre={props.moviesData[5].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[6].title}
+                posterPath={props.moviesData[6].poster_path}
+                releaseDate={props.moviesData[6].release_date}
+                genre={props.moviesData[6].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[7].title}
+                posterPath={props.moviesData[7].poster_path}
+                releaseDate={props.moviesData[7].release_date}
+                genre={props.moviesData[7].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[8].title}
+                posterPath={props.moviesData[8].poster_path}
+                releaseDate={props.moviesData[8].release_date}
+                genre={props.moviesData[8].genres[0]}
+            />
+            <MovieCard
+                screen={props.screen}
+                setScreen={props.setScreen}
+                title={props.moviesData[9].title}
+                posterPath={props.moviesData[9].poster_path}
+                releaseDate={props.moviesData[9].release_date}
+                genre={props.moviesData[9].genres[0]}
+            />
         </>
     );
 }
