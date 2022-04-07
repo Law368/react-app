dcaf714c1020eacde9664ca97574c3a973794d7c
Roman Lysov <Roman_Lysov@epam.com>
2022-04-08 00:47:20 +0400

    routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './style.scss';
 import React from 'react';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import {store} from './store/store';
 import {MoviesList} from './components/Pages/MoviesList';
 import {MovieInfo} from './components/Pages/MovieInfo';
@@ -10,9 +10,8 @@ export function App() {
     return (
         <Provider store={store}>
             <Routes>
-                <Route path="/" element={<MoviesList />}>
-                    <Route path="movie/:id" element={<MovieInfo />} />
-                </Route>
+                <Route path="/" element={<MoviesList />} />
+                <Route path="movie/:id" element={<MovieInfo />} />
             </Routes>
         </Provider>
     );
